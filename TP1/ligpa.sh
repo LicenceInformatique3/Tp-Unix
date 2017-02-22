4d664c933ecb055b0108f4b3e6e746371f0201b6
moonlight83340 <moonlight83340@users.noreply.github.com>
2017-02-22 03:41:38 +0100

    Create ligpa.sh

---
+++ TP1/ligpa.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Donner un nom de fichier : "
+read file
+if test -r $file
+then
+	isPair=false
+	cat fichier | while  read ligne
+	do 
+		if [ "$isPair" = true]
+		then
+			echo $ligne
+			isPair=false
+		else
+			isPair=true
+		fi
+	done
+else
+	echo "Fichier incorrect !"
+	exit 1
+fi
+exit 0
