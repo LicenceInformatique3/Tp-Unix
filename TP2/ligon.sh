fbaf5190c920882339a67a51107388a6f057b596
moonlight83340 <moonlight83340@users.noreply.github.com>
2017-02-22 03:25:17 +0100

    Create ligon.sh

---
+++ TP2/ligon.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if  test $# -ge 1 
+	then
+	for param in "$@"
+	do
+		echo "$param (O/N) ?"
+		read answer
+		if test "$answer" = 'O'
+			then
+			touch tmp
+			echo "$param" >> tmp
+		fi
+	done
+else
+	(>&2 echo "Erreur, il faut des arguments")
+fi
+cat tmp
+rm tmp
